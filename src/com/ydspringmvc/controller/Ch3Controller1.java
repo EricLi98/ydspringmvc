b660d016280eecf9402c6ebd34ded146ff9aa91d
InspurUptec <94768260@qq.com>
2020-05-22 11:47:31 +0800

    0522

--- src/com/ydspringmvc/controller/Ch3Controller1.java
+++ src/com/ydspringmvc/controller/Ch3Controller1.java
@@ -1,17 +1,59 @@
 package com.ydspringmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping(value="/ch31")
 public class Ch3Controller1 {
 	
-	@RequestMapping(value="/ch31a")
+	@RequestMapping(value="/a")
 	public ModelAndView mapa(){
 		ModelAndView mv=new ModelAndView();
 		mv.addObject("name","/ch31a");
 		mv.setViewName("hello");
 		return mv;
 	}
+	
+	@RequestMapping(value={"/b","/b/a"})
+	public ModelAndView mapb(){
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("name","/ch31b");
+		mv.setViewName("hello");
+		return mv;
+	}
+	
+	@RequestMapping(value="/c/{id}/abc/{stuId}")
+	public ModelAndView mapc(@PathVariable(value="id") String userId,@PathVariable(value="stuId") String stuId){
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("name","/ch31c:"+userId+",stuId:"+stuId);
+		mv.setViewName("hello");
+		return mv;
+	}
+	//?代表只能有一个字符
+	@RequestMapping(value="/d?")
+	public ModelAndView mapd(){
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("name","/ch31d");
+		mv.setViewName("hello");
+		return mv;
+	}
+	//*代表0个或多个字符
+	@RequestMapping(value="/e*")
+	public ModelAndView mape(){
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("name","/ch31e");
+		mv.setViewName("hello");
+		return mv;
+	}
+	@RequestMapping(value="/f**")
+	public ModelAndView mapf(){
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("name","/ch31f");
+		mv.setViewName("hello");
+		return mv;
+	}
+	
 }
